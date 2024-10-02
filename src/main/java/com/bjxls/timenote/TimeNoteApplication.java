4568cceca063bd35bf6d7d1a1e65d8cdff10330e
BJXLS <1390177059@qq.com>
2024-10-02 11:40:39 +0800

    project init

---
+++ src/main/java/com/bjxls/timenote/TimeNoteApplication.java
@@ -0,0 +1,13 @@
+package com.bjxls.timenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeNoteApplication.class, args);
+    }
+
+}
